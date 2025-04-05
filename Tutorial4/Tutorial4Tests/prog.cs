9ef4bb6a2425cd0849296def7805040ea07870c8
Michal-Nowakowski21 <s30965@pjwstk.edu.pl>
2025-04-05 20:07:46 +0200

    wersja koncowa

---
+++ Tutorial4/Tutorial4Tests/prog.cs
@@ -0,0 +1,11 @@
+﻿namespace Tutorial3Tests;
+
+public class prog
+{
+    public static void main(string[] args)
+    {
+        EmpDeptSalgradeTests empDeptSalgradeTests = new EmpDeptSalgradeTests();
+        empDeptSalgradeTests.ShouldReturnAllSalesmen();
+       
+    }
+}
